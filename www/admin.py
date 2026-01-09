81c98d7c75b306c24380bff1b8f71980f61c6bef
Seb-mlrd <sebmlrd06@gmail.com>
2026-01-09 15:14:48 +0100

    config admin + requirements

--- www/admin.py
+++ www/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from . import models
+
+class SportSiteAdmin(admin.ModelAdmin):
+    list_display = ('appellation', 'departement', 'commune', 'code_postal', 'site_olympique', 'preview_image')
+    search_fields = ('appellation', 'commune', 'adresse_com')
+    list_filter = ('departement', 'typologie', 'denomination')
+
+    def preview_image(self, obj):
+        if obj.url_image:
+            return format_html('<img src="{}" width="100" height="100" />', obj.url_image)
+        return "No Image"
+
+admin.site.register(models.sportSite, SportSiteAdmin)
+admin.site.register(models.Typo)
+admin.site.register(models.Denomination)
+admin.site.register(models.DateReference)
+admin.site.register(models.site)
+
+    
